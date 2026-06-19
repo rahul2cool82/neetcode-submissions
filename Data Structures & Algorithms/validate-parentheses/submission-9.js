b80232cfeea60cfcdcf70947caa64955961bcdaf
rahul2cool82 <30284164+rahul2cool82@users.noreply.github.com>
2026-06-19 09:11:39 +0530

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const pairBrackets = {
+            '}' : '{',
+            ')' : '(',
+            ']' : '[',
+        }
+
+        const stack = []
+
+        for(const b of s) {
+            if (pairBrackets[b]) {  // If it's a closing bracket
+                if (stack[stack.length - 1] === pairBrackets[b]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {  // Opening bracket
+                stack.push(b);
+            }
+        }
+
+        return stack.length == 0
+
+    }
+}
