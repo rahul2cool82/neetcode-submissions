8642dd94df687d350104ebcbfe2520c08ffd6eac
rahul2cool82 <30284164+rahul2cool82@users.noreply.github.com>
2026-08-22 20:39:56 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const alphabets = "abcdefghijklmnopqrstuvwxyz";
+        const alphabetsmap = {
+
+        }
+
+        const indexedAlphabets = {
+
+        }
+
+        for(let i=0; i<alphabets.length; i++) {
+            alphabetsmap[alphabets[i]] = i;
+            indexedAlphabets[i] = alphabets[i]
+        }
+
+
+        const map = {
+            /**
+             * [sortedstr] = [str1, str2]
+             * 
+             */
+        }
+
+        for(let i=0; i<strs.length; i++) {
+            const arrayStr = new Array(26).fill('')
+
+            const str = strs[i]
+            for(let j=0; j<str.length; j++) {
+                const index = alphabetsmap[str[j]];
+                arrayStr[index] = (arrayStr[index] || '') + str[j]
+            }
+
+            const sortedStr = arrayStr.filter((s) => !!s).join('')
+            if(!map[sortedStr]) {
+                map[sortedStr] = []
+                
+            }
+            (map[sortedStr] || []).push(str);
+            
+
+        }
+
+        return Object.values(map)
+
+
+    }
+}
