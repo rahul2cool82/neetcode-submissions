c00266bcf01f706f6771c3f69575082d03fb8e5c
rahul2cool82 <30284164+rahul2cool82@users.noreply.github.com>
2026-08-23 11:06:26 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let majorityNumber = nums[0];
+        const map = {[majorityNumber]: 1}
+
+        for(let i=1; i<nums.length; i++) {
+            map[nums[i]] = (map[nums[i]] || 0) + 1;
+            majorityNumber = map[majorityNumber] > map[nums[i]] ? majorityNumber : nums[i];
+            console.log({majorityNumber, map})
+        }
+
+        return majorityNumber
+
+    }
+}
