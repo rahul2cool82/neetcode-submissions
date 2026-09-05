49619da1fed2786411a009247d8b5bc9e68438e3
rahul2cool82 <30284164+rahul2cool82@users.noreply.github.com>
2026-09-05 20:22:06 +0530

    Add: first-missing-positive - submission-17

---
+++ Data Structures & Algorithms/first-missing-positive/submission-17.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    firstMissingPositive(nums) {
+        for(let i=0; i<nums.length; i++) {
+            if(nums[i] <= 0) {
+                nums[i] = nums.length + 1
+            }
+        }
+
+        for(let i=0; i<nums.length; i++) {
+            const n = (nums[i] < 0 ? -1*nums[i] : nums[i])-1;
+            if(n>=nums.length) {
+                continue;
+            }
+            nums[n] = nums[n] > 0 ? -1*nums[n] : nums[n]
+        }
+
+
+        for(let i=0; i<nums.length; i++) {
+            if(nums[i] > 0) {
+                return i+1;
+            }
+        }
+
+        return nums.length + 1;
+
+    }
+}
