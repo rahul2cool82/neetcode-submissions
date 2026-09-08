fe55022c0d15e6b478fd54c7720336a2ca43eb9f
rahul2cool82 <30284164+rahul2cool82@users.noreply.github.com>
2026-09-08 08:29:24 +0530

    Add: maximum-frequency-stack - submission-6

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-6.js
@@ -0,0 +1,47 @@
+
+
+class FreqStack {
+    map = {}
+    stack = [[]]
+    constructor() {}
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        const map = this.map;
+        const stack = this.stack;
+
+        map[val] = (map[val] || 0) + 1;
+        if(map[val] == stack.length) {
+            stack.push([])
+        }
+        console.log(stack)
+        stack[map[val]].push(val)
+    }
+
+    /**
+     * @return {number}
+     */
+    pop() {
+        const map = this.map;
+        const stack = this.stack;
+        
+        const topStack = stack[stack.length-1];
+        const val = topStack.pop();
+
+        map[val] = map[val] - 1;
+        if(topStack.length === 0) {
+            stack.pop()
+        }
+        return val
+    }
+}
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * var obj = new FreqStack()
+ * obj.push(val)
+ * var param_2 = obj.pop()
+ */
